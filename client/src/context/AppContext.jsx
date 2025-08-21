bbf0e71050063e46c9eeef8f8bb933dafc965e85
nityanand-kr <nityanandkumar752@gmail.com>
2025-08-21 09:58:15 +0530

    Update AppContext.jsx 1

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import {toast} from 'react-hot-toast'
 import { useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
