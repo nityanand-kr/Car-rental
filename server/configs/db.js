8fbf492013f0a77584a5a4b12048cb7392100195
nityanand-kr <nityanandkumar752@gmail.com>
2025-08-21 10:11:50 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`)
+    await mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     } catch (error) {
         console.log(error.message);
     }
